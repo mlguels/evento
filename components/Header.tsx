0b9cf9a3528d6fbf90c3af27cd3577454e03db05
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-03-10 16:22:35 -0700

    Refactored links now being mapped over

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,31 @@
 import Link from "next/link";
 import Logo from "./Logo";
 
+const routes = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "All Events",
+    path: "/events/all",
+  },
+];
+
 export default function Header() {
   return (
     <header className="flex items-center justify-between border-b border-white/10 h-14 px-3 sm:px-9">
       <Logo />
       <nav>
         <ul className="flex gap-3 text-sm">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/events/all">All Events</Link>
-          </li>
+          {routes.map((route) => (
+            <li
+              key={route.path}
+              className="text-white/50 hover:text-white transition"
+            >
+              <Link href={route.path}>{route.name}</Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
