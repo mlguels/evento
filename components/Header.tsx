67dbd06bca77a9ad365572bdf61304dbf31f25f6
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-03-10 16:15:44 -0700

    Add styling to nav bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,10 +3,18 @@ import Logo from "./Logo";
 
 export default function Header() {
   return (
-    <header>
+    <header className="flex items-center justify-between border-b border-white/10 h-14 px-3 sm:px-9">
       <Logo />
-      <Link href="/">Home</Link>
-      <Link href="/events/all">All Events</Link>
+      <nav>
+        <ul className="flex gap-3 text-sm">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/events/all">All Events</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
