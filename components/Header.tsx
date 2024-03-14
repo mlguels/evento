fb19cd6de1686b602c27ed00bda5e00e9a6cb407
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-03-13 20:19:17 -0700

    Installed framer motion

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,17 +22,24 @@ export default function Header() {
   return (
     <header className="flex items-center justify-between border-b border-white/10 h-14 px-3 sm:px-9">
       <Logo />
-      <nav>
-        <ul className="flex gap-3 text-sm">
+      <nav className="h-full">
+        <ul className="flex h-full gap-3 text-sm">
           {routes.map((route) => (
             <li
               key={route.path}
-              className={clsx("hover:text-white transition", {
-                "text-white": pathname === route.path,
-                "text-white/50": pathname !== route.path,
-              })}
+              className={clsx(
+                "hover:text-white flex items-center transition relative",
+                {
+                  "text-white": pathname === route.path,
+                  "text-white/50": pathname !== route.path,
+                }
+              )}
             >
               <Link href={route.path}>{route.name}</Link>
+
+              {pathname === route.path && (
+                <div className="bg-accent h-1 w-full absolute bottom-0" />
+              )}
             </li>
           ))}
         </ul>
