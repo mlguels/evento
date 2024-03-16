ff522526af8b37cb541439b402c94b6f38c6b425
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-03-16 15:14:54 -0700

    Add page params for dynamic h1

--- app/events/[city]/page.tsx
+++ app/events/[city]/page.tsx
@@ -1,9 +1,21 @@
 import H1 from "@/components/H1";
 
-export default function EventsPage() {
+type EventsPageProps = {
+  params: {
+    city: string;
+  };
+};
+
+export default function EventsPage({ params }: EventsPageProps) {
+  const city = params.city;
+
   return (
-    <main>
-      <H1>Events in Austin</H1>
+    <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
+      <H1>
+        {city === "all" && "All Events"}
+        {city !== "all" &&
+          `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+      </H1>
     </main>
   );
 }
