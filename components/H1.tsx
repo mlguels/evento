5ea78e4f78bbfe1a73c4f970b83a90fec305aa93
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-03-16 16:26:17 -0700

    Add tailwind merge

--- components/H1.tsx
+++ components/H1.tsx
@@ -1,6 +1,18 @@
-export default function H1({ children }: { children: React.ReactNode }) {
+import { twMerge } from "tailwind-merge";
+
+type H1Props = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export default function H1({ children, className }: H1Props) {
   return (
-    <h1 className="text-3xl lg:text-6xl tracking-tight font-bold">
+    <h1
+      className={twMerge(
+        "text-3xl lg:text-6xl tracking-tight font-bold",
+        className
+      )}
+    >
       {children}
     </h1>
   );
