54bb11ea64e511ff6e49e2d4d8392711b422dc7a
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-03-17 14:12:01 -0700

    Add background image of event page

--- app/event/[slug]/page.tsx
+++ app/event/[slug]/page.tsx
@@ -1,3 +1,33 @@
-export default function EventPage() {
-  return <div>EventPage</div>;
+import Image from "next/image";
+
+type EventPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function EventPage({ params }: EventPageProps) {
+  const slug = params.slug;
+
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+  const event = await response.json();
+
+  return (
+    <main>
+      <section className="relative h-[361px] overflow-hidden">
+        <Image
+          src={event.imageUrl}
+          alt="Event background image"
+          fill
+          sizes="(max-width: 1280px) 100vw, 1280px"
+          quality={50}
+          className="object-cover blur-2xl z-0"
+        />
+      </section>
+
+      <div></div>
+    </main>
+  );
 }
