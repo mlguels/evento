c64309eb08f7bf2a4b646e6c5f5f4170f6b8f9d5
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-03-19 19:07:33 -0700

    Add layout + styled event page

--- app/event/[slug]/page.tsx
+++ app/event/[slug]/page.tsx
@@ -17,26 +17,75 @@ export default async function EventPage({ params }: EventPageProps) {
 
   return (
     <main>
-      <section className="relative h-[361px] overflow-hidden">
+      <section
+        className="
+          relative 
+          overflow-hidden 
+          flex 
+          justify-center 
+          items-center 
+          py-14 
+          md:py-20
+        "
+      >
         <Image
           src={event.imageUrl}
           alt="Event background image"
           fill
           sizes="(max-width: 1280px) 100vw, 1280px"
           quality={50}
-          className="object-cover blur-2xl z-0"
+          className="object-cover blur-3xl z-0"
+          priority
         />
 
-        <div className="z-1 relatives">
+        <div className="z-1 flex flex-col lg:flex-row gap-6 lg:gap-16 relatives">
           <Image
             src={event.imageUrl}
             alt={event.name}
             width={300}
             height={201}
+            className="rounded-xl border-2 border-white/50 object-cover"
           />
 
-          <div>
-            <H1>{event.name}</H1>
+          <div className="flex flex-col">
+            <p className="text-white/75">
+              {new Date(event.date).toLocaleDateString("en-US", {
+                weekday: "long",
+                month: "long",
+                day: "numeric",
+              })}
+            </p>
+
+            <H1 className="mb-2 mt-1 whitespace-nowrap lg:text-5xl">
+              {event.name}
+            </H1>
+
+            <p className="whitespace-nowrap text-xl text-white/75">
+              Organized by <span className="italic">{event.organizerName}</span>
+            </p>
+
+            <button
+              className="
+                bg-white/20 
+                text-lg 
+                capitalize 
+                bg-blur
+                mt-5
+                lg:mt-auto
+                w-[95vw] 
+                rounded-md å
+                border-white/10
+                border-2
+                sm:w-full  
+                py-2
+                focus:scale-105
+                hover:scale-105
+                active:scale-[1.02]
+                transition
+              "
+            >
+              Get Tickets
+            </button>
           </div>
         </div>
       </section>
