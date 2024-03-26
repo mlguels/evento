01e024f08b99fe4ccaf31b255370fc471ec1d942
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-03-26 00:34:30 -0700

    Retyped eventsPageProps

--- app/events/[city]/page.tsx
+++ app/events/[city]/page.tsx
@@ -13,6 +13,10 @@ type Props = {
   };
 };
 
+type EventsPageProps = Props & {
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
 export function generateMetadata({ params }: Props): Metadata {
   const city = params.city;
 
@@ -21,8 +25,12 @@ export function generateMetadata({ params }: Props): Metadata {
   };
 }
 
-export default async function EventsPage({ params }: Props) {
+export default async function EventsPage({
+  params,
+  searchParams,
+}: EventsPageProps) {
   const city = params.city;
+  const page = searchParams.page || 1;
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
@@ -32,7 +40,7 @@ export default async function EventsPage({ params }: Props) {
       </H1>
 
       <Suspense fallback={<Loading />}>
-        <EventsList city={city} />
+        <EventsList city={city} page={+page} />
       </Suspense>
     </main>
   );
