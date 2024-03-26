9574e445d1386aa48dae6c067c270c8fc104db21
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-03-25 20:29:46 -0700

    renamed eventslist

---
+++ components/events-list.tsx
@@ -0,0 +1,22 @@
+import { EventoEvent } from "@/lib/types";
+import EventCard from "./event-card";
+
+type EventsListProps = {
+  city: string;
+};
+
+export default async function EventsList({ city }: EventsListProps) {
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
+  );
+
+  const events: EventoEvent[] = await response.json();
+
+  return (
+    <section className="max-w-[1100px] flex flex-wrap gap-10 justify-center px-[20px]">
+      {events.map((event) => (
+        <EventCard key={event.id} event={event} />
+      ))}
+    </section>
+  );
+}
